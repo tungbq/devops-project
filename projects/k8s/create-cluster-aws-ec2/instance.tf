f3a134d4a743e071e2a0f9bc8f2252ebeb2914d7
Tung Bui <tung.bquang@gmail.com>
2023-12-23 14:36:53 +0700

    init the k8s project

---
+++ projects/k8s/create-cluster-aws-ec2/instance.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "modules/ec2"
+
+  instance_name = "k8s-node"
+  ami_id        = "ami-0735c191cf914754d"
+  # instance_type  = "t2.medium"
+  instance_type  = "t2.small"
+  key_name       = "k8sclusteraws"
+  subnet_ids     = ["subnet-058a7514ba8adbb07", "subnet-0dbcd1ac168414927", "subnet-032f5077729435858"]
+  instance_count = 3
+
+  inbound_rules = [
+    {
+      from_port  = 0
+      to_port    = 65000
+      protocol   = "TCP"
+      cidr_block = "172.31.0.0/16"
+    },
+    {
+      from_port  = 6443
+      to_port    = 6443
+      protocol   = "TCP"
+      cidr_block = "0.0.0.0/0"
+    },
+    {
+      from_port  = 22
+      to_port    = 22
+      protocol   = "TCP"
+      cidr_block = "0.0.0.0/0"
+    },
+    {
+      from_port  = 30000
+      to_port    = 32768
+      protocol   = "TCP"
+      cidr_block = "0.0.0.0/0"
+    }
+  ]
+
+  outbound_rules = [
+    {
+      from_port  = 0
+      to_port    = 0
+      protocol   = "-1"
+      cidr_block = "0.0.0.0/0"
+    }
+  ]
+}
---
+++ projects/k8s/create-cluster-aws-ec2/modules/ec2/main.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "instance-sg" {
+  name        = "K8s Node SG"
+  description = "SG for K8s Nodes"
+
+  dynamic "ingress" {
+    for_each = var.inbound_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = [ingress.value.cidr_block]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.outbound_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = [egress.value.cidr_block]
+    }
+  }
+}
+
+resource "aws_instance" "example" {
+  count = var.instance_count
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.instance-sg.id]
+
+  user_data = <<-EOF
+    #cloud-config
+    hostname: ${count.index == 0 ? "controlplane" : "node0${count.index}"}
+  EOF
+
+  tags = {
+    Name = count.index == 0 ? "controlplane" : "node0${count.index}"
+  }
+
+  subnet_id = element(var.subnet_ids, count.index % length(var.subnet_ids))
+}
