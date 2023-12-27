aac96ffb6ed1743cae41dffe783a661dd480e44e
Tung Bui <tung.bquang@gmail.com>
2023-12-27 22:14:12 +0700

    fix curl 52 issue :bug:

--- projects/dockerize-python-application/demo_project.sh
+++ projects/dockerize-python-application/demo_project.sh
@@ -4,5 +4,8 @@ docker build -t my-flask-app .
 # Run the Docker container based on the image
 docker run -d -p 5000:5000 my-flask-app
 
+echo "Wait 20s for application up!"
+sleep 20
+
 # Verify result
 curl localhost:5000
