3752ac2c02f2052516e04bdbc23efa34c13e5cd4
Tung Bui <tung.bquang@gmail.com>
2023-12-27 22:57:50 +0700

    Add package.json

--- projects/tree-tier-app-k8s/demo_script.sh
+++ projects/tree-tier-app-k8s/demo_script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+pwd
 echo "Build frontend"
-docker build -t your-frontend-image:v1 .
+pushd frontend
+docker build -t react-frontend .
+docker run -p 3000:80 -e REACT_APP_API_URL='http://localhost:5000' react-frontend
+popd
+
+pwd
