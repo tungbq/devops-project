f515a49a70432cf4a603ffe7c5bdd46cfe2bb0ac
Tung Bui <tung.bquang@gmail.com>
2023-12-27 22:52:05 +0700

    add backend code

---
+++ projects/tree-tier-app-k8s/backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors'); // For handling CORS (Cross-Origin Resource Sharing)
+const app = express();
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+	res.json({ message: 'Hello from the Node.js backend!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
