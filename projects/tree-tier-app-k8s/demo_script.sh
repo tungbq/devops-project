f0a6e63ae11212aab9367f85ffbaf045521ea301
Tung Bui <tung.bquang@gmail.com>
2023-12-27 23:06:00 +0700

    Add index html

--- projects/tree-tier-app-k8s/demo_script.sh
+++ projects/tree-tier-app-k8s/demo_script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Backend
+pwd
+echo "Build backend"
+pushd backend
+docker build -t node-backend .
+docker run -d -p 5000:5000 node-backend
+popd
+
+# Frontend
 pwd
 echo "Build frontend"
 pushd frontend
@@ -7,4 +16,4 @@ docker build -t react-frontend .
 docker run -p 3000:80 -e REACT_APP_API_URL='http://localhost:5000' react-frontend
 popd
 
-pwd
+# DB
