c159f23d8fed646a88a89ee495e632a17f866f59
Tung Bui <tung.bquang@gmail.com>
2023-12-29 18:04:52 +0700

    fix frontend build

---
+++ projects/tree-tier-app-k8s/frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming your main component is named App and located in App.js
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
