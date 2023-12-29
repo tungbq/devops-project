4721d30db8e62d2a4105c169077a0483cdba5f10
Tung Bui <tung.bquang@gmail.com>
2023-12-29 18:12:00 +0700

    fix frontend build

--- projects/tree-tier-app-k8s/frontend/src/App.js
+++ projects/tree-tier-app-k8s/frontend/src/App.js
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    fetch('http://backend:3001/api/tasks') // Fetch tasks from the backend
-      .then((res) => res.json())
-      .then((data) => setTasks(data))
-      .catch((error) => console.error('Error:', error));
-  }, []);
+	useEffect(() => {
+		fetch('process.env.REACT_APP_API_URL' + '/api/tasks') // Fetch tasks from the backend
+			.then((res) => res.json())
+			.then((data) => setTasks(data))
+			.catch((error) => console.error('Error:', error));
+	}, []);
 
-  return (
-    <div>
-      <h1>Task Management</h1>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>{task.title}</li>
-          // Other task details rendering...
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Task Management</h1>
+			<ul>
+				{tasks.map((task) => (
+					<li key={task.id}>{task.title}</li>
+					// Other task details rendering...
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default App;
