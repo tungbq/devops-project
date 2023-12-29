df52a5d9cf50efbfe9631f2084d04c0a64467048
Tung Bui <tung.bquang@gmail.com>
2023-12-29 23:03:45 +0700

    fix test.v1

--- projects/nodejs-cicd-pipeline/server.js
+++ projects/nodejs-cicd-pipeline/server.js
@@ -29,3 +29,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = { app, pool }; // Export app and other necessary elements
