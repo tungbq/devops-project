560c34c24b5dc32b97af2e07a7007494abede95c
Tung Bui <tung.bquang@gmail.com>
2024-01-28 11:48:57 +0700

    p06: add public access IP

--- projects/terraform-aws-windows-instance/main.tf
+++ projects/terraform-aws-windows-instance/main.tf
@@ -7,6 +7,6 @@ module "ec2_instance" {
   source        = "./modules/ec2"
   instance_type = "t2.micro"              # Change as needed
   ami           = "ami-00d990e7e5ece7974" # Windows server 2022
-  key_name      = "demo-window-ec2"
-  allowed_ip    = "1.2.3.4" # Specify the allowed IP address for RDP
+  key_name      = "demo-windows-ec2"
+  allowed_ip    = var.allowed_ip # Specify the allowed IP address for RDP (from terraform.tfvars)
 }
