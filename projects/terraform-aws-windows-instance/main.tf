eb284a802f2a42a2ff77a4ce0a6f519be3097962
Tung Bui <tung.bquang@gmail.com>
2024-01-28 11:35:30 +0700

    p06: add ami

--- projects/terraform-aws-windows-instance/main.tf
+++ projects/terraform-aws-windows-instance/main.tf
@@ -6,5 +6,5 @@ provider "aws" {
 module "ec2_instance" {
   source        = "./modules/ec2"
   instance_type = "t2.micro"              # Change as needed
-  ami           = "ami-xxxxxxxxxxxxxxxxx" # Specify your Windows AMI ID
+  ami           = "ami-00d990e7e5ece7974" # Windows server 2022
 }
