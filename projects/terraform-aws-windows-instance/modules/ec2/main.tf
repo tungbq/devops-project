718ca5f8182c6f21132d11e923bb71cb0471a057
Tung Bui <tung.bquang@gmail.com>
2024-01-28 13:44:20 +0700

    [issue-38] windows ec2 - attach security group.outbound

--- projects/terraform-aws-windows-instance/modules/ec2/main.tf
+++ projects/terraform-aws-windows-instance/modules/ec2/main.tf
@@ -4,11 +4,20 @@ resource "aws_security_group" "ec2_security_group" {
   name        = "ec2_security_group"
   description = "Allow RDP access from a specific IP address"
 
+  # Only allow inbound RDP access from specific IP
   ingress {
     from_port   = 3389
     to_port     = 3389
     protocol    = "tcp"
     cidr_blocks = ["${var.allowed_ip}/32"]
+  }
+
+  # Allow the instance to connect to the world
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Add other security group configurations as needed
